0a346d410e2a6211ff348aa0822d7a0e0328139d
Joe Kutner <jpkutner@gmail.com>
2020-12-05 02:30:43 -0600

    Fix CI

--- test/hello-world-js.sh
+++ test/hello-world-js.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 confirm_output() {
+  docker ps -aq | xargs docker kill
   docker run --rm -p 9000:8080 hello-world-js &
   sleep 3
   output=$(curl -s -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}')
@@ -17,12 +18,12 @@ confirm_output() {
 
 echo "=== TEST jkutner/aws-lambda-builder:18"
 pack trust-builder jkutner/aws-lambda-builder:18
-pack build --clear-cache --builder jkutner/aws-lambda-builder:18 hello-world-js --path fixtures/hello-world-js --pull-policy never
+pack build --clear-cache --builder jkutner/aws-lambda-builder:18 hello-world-js --path fixtures/hello-world-js --pull-policy if-not-present
 confirm_output
 echo "SUCCESS"
 
 echo "=== TEST heroku/buildpacks:18"
 docker pull heroku/buildpacks:18
-pack build --clear-cache --builder heroku/buildpacks:18 --buildpack jkutner/lambda-cnb hello-world-js --path fixtures/hello-world-js --pull-policy never
+pack build --clear-cache --builder heroku/buildpacks:18 --buildpack jkutner/aws-lambda-cnb hello-world-js --path fixtures/hello-world-js --pull-policy if-not-present
 confirm_output
 echo "SUCCESS"
